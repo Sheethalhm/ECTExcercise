1ec8a3934fbc95bbb2744a69667e59f0d85bafb7
n448211 <sheethal.holenarasipura@pagonxt.com>
2024-07-24 23:27:57 +0200

    Cypress Cloud integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '93uv1q',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
